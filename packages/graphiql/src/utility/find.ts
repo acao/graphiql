7f4d9cde7b409b9e779e02b6dc543f6ccf9594b9
Alec Aivazis <alec@aivazis.com>
2019-06-18 12:50:36 -0700

    find types

--- packages/graphiql/src/utility/find.ts
+++ packages/graphiql/src/utility/find.ts
@@ -9,10 +9,12 @@
 export default function find<T>(
   list: Array<T>,
   predicate: (item: T) => boolean,
-): ?T {
+): T | null {
   for (let i = 0; i < list.length; i++) {
     if (predicate(list[i])) {
       return list[i];
     }
   }
+
+  return null;
 }
